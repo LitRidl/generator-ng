52c78ff2553873344ac7c7aa2a087619ea457ef3
LitRidl <litridl@gmail.com>
2015-09-11 17:47:02 +0300

    Removed redundant code in all index.js files

--- partial/index.js
+++ partial/index.js
@@ -4,12 +4,9 @@ var ScriptBase = require('../app/script-base.js'),
 module.exports = ScriptBase.extend({
     constructor: function () {
         ScriptBase.apply(this, arguments);
-        this.option('html-type', {
-            desc: 'Generate a jade partial',
-            type: String
-        }); // This method adds support for a `--jade` flag
     },
+
     exec: function () {
         this._addPartials();
-    }
+    },
 });
